3c69bd6c2a53e85ec191ab16c383595e0d900b94
abhinarwal0001@gmail.com <abhinarwal0001@gmail.com>
2025-07-29 19:00:58 +0530

    Added pipeline

--- script.py
+++ script.py
@@ -4,35 +4,35 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 def send_email(workflow_name, repo_name, workflow_run_id):
-    sender_email = os.getenv('SENDER_EMAIL')
-    sender_password = os.getenv('SENDER_PASSWORD')
+    smtp_server = os.getenv('SMTP_SERVER')
+    smtp_port = int(os.getenv('SMTP_PORT'))
+    smtp_username = os.getenv('SMTP_USERNAME')
+    smtp_password = os.getenv('SMTP_PASSWORD')
     receiver_email = os.getenv('RECEIVER_EMAIL')
 
-    subject = f"Workflow {workflow_name} in {repo_name} has failed"
-    body = (
-        f"The workflow '{workflow_name}' in the repository '{repo_name}' has encountered an error.\n"
-        f"More details can be found in the logs:\nRun_ID: {workflow_run_id}"
-    )
+    subject = f"Workflow {workflow_name} in {repo_name} has finished"
+    body = f"The workflow '{workflow_name}' in the repository '{repo_name}' has completed.\nRun_ID: {workflow_run_id}"
 
     msg = MIMEMultipart()
-    msg['From'] = sender_email
+    msg['From'] = smtp_username
     msg['To'] = receiver_email
     msg['Subject'] = subject
+
     msg.attach(MIMEText(body, 'plain'))
 
     try:
-        server = smtplib.SMTP('smtp.gmail.com', 587)   # Use your SMTP server and port
+        server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()
-        server.login(sender_email, sender_password)
-        server.sendmail(sender_email, receiver_email, msg.as_string())
+        server.login(smtp_username, smtp_password)
+        text = msg.as_string()
+        server.sendmail(smtp_username, receiver_email, text)
         server.quit()
         print(f"✅ Email sent successfully to {receiver_email}")
     except Exception as e:
         print(f"❌ Error sending email: {e}")
 
-if __name__ == "__main__":
-    send_email(
-        os.getenv('WORKFLOW_NAME'),
-        os.getenv('GITHUB_REPOSITORY'),
-        os.getenv('GITHUB_RUN_ID')
-    )
+send_email(
+    os.getenv('WORKFLOW_NAME'),
+    os.getenv('GITHUB_REPOSITORY'),
+    os.getenv('GITHUB_RUN_ID')
+)
